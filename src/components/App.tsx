ef48e45316efa926b47f1fa3f008cc5f97163947
andrewgremlich <andrew.gremlich@gmail.com>
2024-12-01 17:06:39 -0700

    labeling main page for react

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,7 +3,10 @@ import { H1, P } from "@/Text";
 export const App = () => {
 	return (
 		<div>
-			<H1>Hello world</H1>
+			<H1>Summit Kit - React</H1>
+
+			<P>On this page is the React Component Library for the Summit Kit.</P>
+
 			<P>
 				Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate
 				earum, odio maxime ad sapiente porro neque. Dolor expedita quam sint sed
