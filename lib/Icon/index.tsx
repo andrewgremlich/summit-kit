5a8be02db8d0c23054ac8cdb8d25b869595b31c1
andrewgremlich <andrew.gremlich@gmail.com>
2024-12-31 21:32:17 -0700

    adding classes to icon

--- lib/Icon/index.tsx
+++ lib/Icon/index.tsx
@@ -2,16 +2,19 @@ import { P } from "@/Text";
 import type { IconType } from "react-icons";
 import * as Icons from "react-icons/fi";
 
+import type { ClassesProps } from "@/Types";
+
 type IconProps = {
 	name: string;
 	size?: number;
 	color?: string;
-};
+} & ClassesProps;
 
 export const Icon = ({
 	name,
 	size = 24,
 	color = "currentColor",
+	...props
 }: IconProps) => {
 	const IconComponent: IconType = (Icons as { [key: string]: IconType })[name];
 
@@ -19,5 +22,15 @@ export const Icon = ({
 		return <P>Icon not found</P>;
 	}
 
-	return <IconComponent size={size} color={color} />;
+	return (
+		<IconComponent
+			className={`${
+				props.classes && props.classes.length > 0
+					? ` ${props.classes.join(" ")}`
+					: ""
+			}`}
+			size={size}
+			color={color}
+		/>
+	);
 };
